b6ff084fbe03390f8724c07439c230f04c1ee772
Kala Foods <guhan.delta@gmail.com>
2020-12-31 13:08:17 -0500

    Provided a Company Schema and a Resolver() for company. Added a Company field to Job Type, to connect the 2 Objects

--- server/resolvers.js
+++ server/resolvers.js
@@ -4,6 +4,19 @@ const Query = {
     jobs: () => db.jobs.list(),
 };
 
+// Resolvers should adhere to the schema and as Company is a field of Job Type, a new resolver Obj should be defn-
+//- to resolve the fields of a Job, the field that needs ot be resolved is the Company
+const Job = {
+    // Since a company needs to be resolved for a job, resolver() would require the parent Obj to accomplish it
+    // The Job Obj passed to teh company resolver is the response for invoking the parent resolver in the hierarchy
+    
+    // When a client requests for a company, the Jobs resolver will be called, which will return an array of Job-
+    //- objects. The GraphQL engine will traverse through all these objects to invoke the company resolver for-
+    //- the Obj type 
+    company: (job) => db.companies.get(job.companyId)
+};
+
 module.exports = {
-    Query
+    Query,
+    Job
 };
