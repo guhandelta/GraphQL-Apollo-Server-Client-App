70ea8d7dfccc6907cae93d635005993416d51a02
Kala Foods <guhan.delta@gmail.com>
2020-12-31 09:28:28 -0500

    Separated the Schema and Resolver logic into separate files for code modularity, with some basic stuff to get the server up and running

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const expressJwt = require('express-jwt');
 const jwt = require('jsonwebtoken');
+const { ApolloServer, gql } = require('apollo-server-express');
 const db = require('./db');
 
 const port = 9000;
@@ -13,6 +15,17 @@ app.use(cors(), bodyParser.json(), expressJwt({
   secret: jwtSecret,
   credentialsRequired: false
 }));
+
+// Keeping the code modular by separating the Schema and Resolver Logic
+// gql can be used as a fn here, instead of using it as a template literal
+// utf-8 encoding is provided to make sure the file is read as a string and not as a binary file
+const typeDefs =  gql(fs.readFileSync('./schema.graphql', { encoding: 'utf8' }));
+
+const resolvers = require('./resolvers')
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+// applyMiddleware() => Integrates Apollo server into the Express app, instead of running it as a standalone server
+apolloServer.applyMiddleware({ app, path: '/graphql' }); 
 
 app.post('/login', (req, res) => {
   const {email, password} = req.body;
